e7cb82fc90d5ae157dc07ec51df7cc9b294cd88a
Marek Pechal <marek.pechal@gmail.com>
2024-02-21 11:57:57 +0100

    Redefined footprint such that its port faces in the +x direction.

--- KiCadUtils/Footprints.py
+++ KiCadUtils/Footprints.py
@@ -17,12 +17,12 @@ class SMP_19S102_40ML5_2layer_508um_AD1000(KiCadStructure):
 
         # adding vias
         footprint_via_positions = [
-            [-2.0, -2.0], [-2.0, -1.0], [-2.0, 0.0], [-2.0, 1.0], [-2.0, 2.0],
-            [+2.0, -2.0], [+2.0, -1.0], [+2.0, 0.0], [+2.0, 1.0], [+2.0, 2.0],
-            [-1.0, 2.0], [0.0, 2.0], [1.0, 2.0],
-            [-0.45, -4.0], [-1.05, -3.0], [-1.05, -2.0], [-0.85, -1.0], [-0.85, 0.0],
-            [+0.45, -4.0], [+1.05, -3.0], [+1.05, -2.0], [+0.85, -1.0], [+0.85, 0.0],
-            [-0.601, 0.601], [0.0, 0.85], [+0.601, 0.601]
+            [+2.0, -2.0], [+1.0, -2.0], [0.0, -2.0], [-1.0, -2.0], [-2.0, -2.0],
+            [+2.0, +2.0], [+1.0, +2.0], [0.0, +2.0], [-1.0, +2.0], [-2.0, +2.0],
+            [-2.0, -1.0], [-2.0, 0.0], [-2.0, 1.0],
+            [+4.0, -0.45], [+3.0, -1.05], [+2.0, -1.05], [+1.0, -0.85], [0.0, -0.85],
+            [+4.0, +0.45], [+3.0, +1.05], [+2.0, +1.05], [+1.0, +0.85], [0.0, +0.85],
+            [-0.601, -0.601], [-0.85, 0.0], [-0.601, +0.601]
             ]
         for pt in footprint_via_positions:
             self.addAsChild(KiCadStructure.pad_th(
@@ -30,21 +30,21 @@ class SMP_19S102_40ML5_2layer_508um_AD1000(KiCadStructure):
 
         # pad
         self.addAsChild(KiCadStructure.pad_rect(
-            [0, -2.45, 90], 1, 1.1, 0.44, ['F.Cu'], 0, '""'))
+            [2.45, 0, 0], 1, 1.1, 0.44, ['F.Cu'], 0, '""'))
 
         # central keepout
         r = 0.6
         bridge_width = 0.9
-        y1 = -1.45
-        y2 = -3.3
+        x1 = 1.45
+        x2 = 3.3
         cutout_width = 1.4
         theta = np.arcsin(0.5*bridge_width/r)
-        pts = [[r*np.sin(u), -r*np.cos(u)]
+        pts = [[r*np.cos(u), r*np.sin(u)]
             for u in np.linspace(theta, 2*np.pi-theta, 40)]
         pts = (
-            [[cutout_width/2, y2], [cutout_width/2, y1], [bridge_width/2, y1]] +
+            [[x2, cutout_width/2], [x1, cutout_width/2], [x1, bridge_width/2]] +
             pts +
-            [[-bridge_width/2, y1], [-cutout_width/2, y1], [-cutout_width/2, y2]]
+            [[x1, -bridge_width/2], [x1, -cutout_width/2], [x2, -cutout_width/2]]
             )
         self.addAsChild(KiCadStructure.ruleArea(
             'F.Cu', ['copperpour', 'footprints'], pts))
@@ -55,20 +55,20 @@ class SMP_19S102_40ML5_2layer_508um_AD1000(KiCadStructure):
         bridge_width = 1.0
         pts = [
             [solder_area_size/2, solder_area_size/2],
-            [solder_area_size/2, bridge_width/2],
-            [solder_area_size/2+keepout_width, bridge_width/2],
-            [solder_area_size/2+keepout_width, solder_area_size/2+keepout_width],
-            [bridge_width/2, solder_area_size/2+keepout_width],
             [bridge_width/2, solder_area_size/2],
+            [bridge_width/2, solder_area_size/2+keepout_width],
+            [solder_area_size/2+keepout_width, solder_area_size/2+keepout_width],
+            [solder_area_size/2+keepout_width, bridge_width/2],
+            [solder_area_size/2, bridge_width/2],
             ]
         self.addAsChild(KiCadStructure.ruleArea(
             'F.Cu', ['copperpour', 'footprints'], pts))
         self.addAsChild(KiCadStructure.ruleArea(
-            'F.Cu', ['copperpour', 'footprints'], [[-x, y] for x, y in pts]))
-        pts = [[max(x, bridge_width+cutout_width/2), -y] for x, y in pts]
+            'F.Cu', ['copperpour', 'footprints'], [[x, -y] for x, y in pts]))
+        pts = [[-x, max(y, bridge_width+cutout_width/2)] for x, y in pts]
         self.addAsChild(KiCadStructure.ruleArea(
             'F.Cu', ['copperpour', 'footprints'], pts))
         self.addAsChild(KiCadStructure.ruleArea(
-            'F.Cu', ['copperpour', 'footprints'], [[-x, y] for x, y in pts]))
+            'F.Cu', ['copperpour', 'footprints'], [[x, -y] for x, y in pts]))
 
         # END FOOTPRINT DEFINITION

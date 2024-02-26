cc8eeb48a584bf45d70171d45830b7f113a5afbf
Marek Pechal <marek.pechal@gmail.com>
2024-02-26 22:15:00 +0100

    Moving non-essential helper functions into a separate file `helper.py`.

--- KiCadUtils/examples/_testing_QudevPCBs/_test_generate_PCB.py
+++ KiCadUtils/examples/_testing_QudevPCBs/_test_generate_PCB.py
@@ -14,43 +14,13 @@ from matplotlib.collections import PatchCollection
 import shapely
 import shapely.geometry, shapely.ops
 
+from helper import plotShapelyPolygon, plotShapelyPolyLike
 from KiCadStructure import KiCadStructure, _rotation_matrix
 from lqd_routing import RouteDescription
 import Footprints
 import numpy as np
 import json
 import itertools
-
-def plotShapelyPolygon(ax, poly, **kwargs):
-    """
-    Helper function to plot a shapely.geometry.Polygon object
-    """
-    # from https://stackoverflow.com/a/70533052
-    A = np.array([1, -1])
-    path = Path.make_compound_path(
-        Path(np.asarray(poly.exterior.coords)[:, :2] * A),
-        *[Path(np.asarray(ring.coords)[:, :2] * A) for ring in poly.interiors])
-
-    patch = PathPatch(path, **kwargs)
-    collection = PatchCollection([patch], **kwargs)
-
-    ax.add_collection(collection, autolim=True)
-    ax.autoscale_view()
-    return collection
-
-def plotShapelyPolyLike(ax, poly, **kwargs):
-    """
-    Helper function to plot a shapely.geometry.Polygon or
-    shapely.geometry.MultiPolygon object
-    """
-    if isinstance(poly, shapely.geometry.Polygon):
-        polys = [poly]
-    elif isinstance(poly, shapely.geometry.MultiPolygon):
-        polys = poly.geoms
-    else:
-        raise NotImplementedError(f'{poly}')
-    for poly in polys:
-        plotShapelyPolygon(ax, poly, **kwargs)
 
 def getParam(dct, key, namespace):
     return eval(str(dct[key]), namespace)
